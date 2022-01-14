8e280e811dff79856efc3e7a1cde53ec9796816c
ewertonrb <ewerton_tnt@hotmail.com>
2022-01-14 13:23:57 -0300

    erro no Deploy do Front

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -9,11 +9,12 @@ function Navbar() {
       <nav className="container">
         <div className='dsmovie-nav-content'>
           <h1>DSMovie</h1>
-          <a href="https://github.com/ewertonrb"></a>
+          <a href="https://www.github.com/ewertonrb">
           <div className='dsmovie-contact-container'>
             <GithubIcon />
             <p className='dsmovie-contact-link'>/ewertonrb</p>
           </div>
+          </a>
         </div>
       </nav>
     </header>
